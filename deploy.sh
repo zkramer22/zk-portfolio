821e879304b0473d5d40cde4cc0d2af0ffd77210
zach kramer <zkramer22@gmail.com>
2022-06-10 20:52:00 -0700

    trying to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,14 @@
-# !/usr/bin/env sh
-
+#!/usr/bin/env sh
 # abort on errors
 set -e
-
 # build
 npm run build
-
 # navigate into the build output directory
 cd dist
-
 # if you are deploying to a custom domain
-echo 'zkramer.me' > CNAME
-
+echo 'www.example.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:zkramer22/zkramer22.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/zkramer22/zk-portfolio.git master:gh-pages
-
+git push -f git@github.com:username/hello-world.git master:gh-pages
 cd -
